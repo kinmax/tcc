73c786faebdecf9755293029f168d9a43a9333c4
Kin Gusmão <gusmaokin@gmail.com>
2019-06-14 22:58:54 -0300

    Add kitchen patch to dataset_formatter

--- dataset_formatter.rb
+++ dataset_formatter.rb
@@ -2,7 +2,7 @@
 
 require 'byebug'
 
-dataset_path = "/home/kin/dataset-copy"
+dataset_path = "/home/kingusmao/dataset-copy"
 untarcmd = "tar -xjf "
 tarcmd = "tar cvfj "
 percentages = %w(10 30 50 70 100).freeze
@@ -22,17 +22,27 @@ Dir.foreach(dataset_path) do |domain|
             if tar == "." || tar == ".." || tar == "README.md" || tar == ".gitignore" || tar.include?("FILTERED")
                 next
             end
-            puts tar
-            system(untarcmd + "#{dataset_path}/#{domain}/#{percent}/#{tar} > /dev/null" )
-            if(domain == "blocks-world")
-                domain_file = File.open("domain.pddl", "r")
-                dom = domain_file.read
-                domain_file.close
-                dom.gsub!("-block", "- block")
-                File.write("domain.pddl", dom)
+            if domain == "blocks-world" || domain == "kitchen"
+                puts tar
+                system(untarcmd + "#{dataset_path}/#{domain}/#{percent}/#{tar} > /dev/null" )
+                if(domain == "blocks-world")
+                    domain_file = File.open("domain.pddl", "r")
+                    dom = domain_file.read
+                    domain_file.close
+                    dom.gsub!("-block", "- block")
+                    File.write("domain.pddl", dom)
+                end
+                if(domain == "kitchen")
+                    domain_file = File.open("domain.pddl", "r")
+                    dom = domain_file.read
+                    domain_file.close
+                    dom.gsub!("water_jug keetle cloth tea_bag cup sugar bowl milk", "water_jug keetle cloth tea_bag bowl milk")
+                    dom.gsub!("bread toaster butter knife peanut_butter spoon", "butter knife peanut_butter spoon")
+                    File.write("domain.pddl", dom)
+                end
+                #system("ruby problem_formatter.rb template.pddl real_hyp.dat template.pddl > /dev/null")
+                system(tarcmd + "#{dataset_path}/#{domain}/#{percent}/#{tar} *.dat *.pddl > /dev/null")
             end
-            #system("ruby problem_formatter.rb template.pddl real_hyp.dat template.pddl > /dev/null")
-            system(tarcmd + "#{dataset_path}/#{domain}/#{percent}/#{tar} *.dat *.pddl > /dev/null")
         end
     end
 end

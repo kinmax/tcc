6282732d111e14c1b16ebd72a6b65776134a830e
Kin Gusmão <gusmaokin@gmail.com>
2019-06-18 23:56:32 -0300

    Table

---
+++ merge_results.rb
@@ -0,0 +1,29 @@
+require 'json'
+
+path = "/home/kingusmao/t2-integradora"
+output_path = "/home/kingusmao/t2-integradora/results.json"
+
+domains = %w(blocks-world campus depots driverlog dwr easy-ipc-grid ferry intrusion-detection kitchen logistics miconic rovers satellite sokoban zeno-travel).freeze
+
+res = {}
+
+Dir.foreach(path) do |domain|
+    unless(domains.include?(domain))
+        next
+    end
+    Dir.foreach("#{path}/#{domain}") do |file_name|
+        unless(file_name == "results.json")
+            next
+        end
+
+        file = File.open("#{path}/#{domain}/#{file_name}", "r")
+        raw = file.read
+        file.close
+        hash = JSON.parse(raw)
+        underscore_domain = domain.gsub("-", "_")
+        res[domain] = hash[underscore_domain]
+    end
+end
+res = res.sort.to_h
+json_res = JSON.pretty_generate(res)
+File.write(output_path, json_res)
